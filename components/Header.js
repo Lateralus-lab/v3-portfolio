b4fdec81fef394ca6aa52b10e8b973e9c236c161
Lateralus <zoofoo.online@gmail.com>
2021-05-06 22:37:30 +0100

    switch added and light mode od the website implomentes. adjusted svg animations

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,52 @@
+import { useState } from 'react';
+import MoonDark from './svg/switch/moonDark';
+import MoonWhite from './svg/switch/moonWhite';
+import SunDark from './svg/switch/sunDark';
+import SunWhite from './svg/switch/sunWhite';
+
 export default function Header() {
+  const [toggle, setToggle] = useState(false);
+
+  const handleChange = () => {
+    document.body.classList.toggle('dark');
+    setToggle(!toggle);
+  };
+
+  const sunToggle = toggle ? <SunWhite /> : <SunDark />;
+  const moonToggle = toggle ? <MoonWhite /> : <MoonDark />;
+
   return (
     <header className="header">
-      <div className="header__name">
-        <div className="header__name-first">
-          El<span className="multi-color">i</span>
+      <div className="header-left">
+        <div className="header__name">
+          <div className="header__name-first">
+            El<span className="multi-color">i</span>
+          </div>
+          <div className="header__name-second">
+            <span className="multi-color">Kri</span>zevski
+          </div>
         </div>
-        <div className="header__name-second">
-          <span className="multi-color">Kri</span>zevski
+        <div className="header__desc">
+          I'm a passionate front-end web developer focused on building beautiful
+          interfaces and expiriences.
         </div>
       </div>
-      <div className="header__desc">
-        I'm a passionate front-end web developer focused on building beautiful
-        interfaces and expiriences.
+
+      <div className="headr-right">
+        <div className="switch-wrapper">
+          <div className="sun">{sunToggle}</div>
+          <div className="toggle-wrapper">
+            <input
+              id="switch"
+              type="checkbox"
+              onChange={() => handleChange()}
+            />
+            <label htmlFor="switch" id="toggle">
+              Toggle
+            </label>
+          </div>
+          <div className="moon">{moonToggle}</div>
+        </div>
       </div>
     </header>
   );
