01d13b98810ae05b554ad865cd4c29425067c952
Lateralus <zoofoo.online@gmail.com>
2021-05-07 15:45:32 +0100

    removed skills section and merged it with about section

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import Header from '../components/Header';
 import Socials from '../components/Socials';
 import Projects from '../components/Projects';
 import Lab from '../components/Lab';
-import Skills from '../components/Skills';
 import Footer from '../components/Footer';
 import About from '../components/About';
 
@@ -14,7 +13,6 @@ export default function Home() {
       <Socials />
       <Projects />
       <Lab />
-      <Skills />
       <About />
       <Footer />
     </Layout>
