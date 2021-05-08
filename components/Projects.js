fbb4a96f7ca216a7d19a8d8f282cfe5ef2a7c71d
Lateralus <zoofoo.online@gmail.com>
2021-05-08 19:28:22 +0100

    made some changes to cards

--- components/Projects.js
+++ components/Projects.js
@@ -8,7 +8,7 @@ export default function Projects() {
   const responsive = {
     576: { items: 1 },
     768: { items: 2 },
-    992: { items: 3 },
+    992: { items: 2 },
   };
 
   const items = [
@@ -19,27 +19,9 @@ export default function Projects() {
           src="/../public/images/cards/chat-app.jpeg"
           alt="temp"
           width={450}
-          height={520}
+          height={540}
           objectFit="cover"
         />
-      </div>
-      <div className="cards__content">
-        <div className="cards__top">
-          <div className="cards__top-title">
-            <h3 className="cards__title">
-              <Link href="#">
-                <a className="cards__title-link">Chat App</a>
-              </Link>
-            </h3>
-          </div>
-          <div className="cards__top-icon">
-            <External />
-          </div>
-        </div>
-        <p>
-          Dolor impedit magnam voluptas quo nobis Eius esse explicabo totam
-          eaque eligendi Ut quisquam quas similique fuga atque obcaecati{' '}
-        </p>
       </div>
     </div>,
 
@@ -50,27 +32,9 @@ export default function Projects() {
           src="/../public/images/cards/loft-taxi.jpeg"
           alt="temp"
           width={450}
-          height={520}
+          height={540}
           objectFit="cover"
         />
-      </div>
-      <div className="cards__content">
-        <div className="cards__top">
-          <div className="cards__top-title">
-            <h3 className="cards__title">
-              <Link href="#">
-                <a className="cards__title-link">Taxi Ordering App</a>
-              </Link>
-            </h3>
-          </div>
-          <div className="cards__top-icon">
-            <External />
-          </div>
-        </div>
-        <p>
-          Dolor impedit magnam voluptas quo nobis Eius esse explicabo totam
-          eaque eligendi Ut quisquam quas similique fuga atque obcaecati{' '}
-        </p>
       </div>
     </div>,
 
@@ -81,27 +45,9 @@ export default function Projects() {
           src="/../public/images/cards/logistics.jpeg"
           alt="temp"
           width={450}
-          height={520}
+          height={540}
           objectFit="cover"
         />
-      </div>
-      <div className="cards__content">
-        <div className="cards__top">
-          <div className="cards__top-title">
-            <h3 className="cards__title">
-              <Link href="#">
-                <a className="cards__title-link">Landing Page</a>
-              </Link>
-            </h3>
-          </div>
-          <div className="cards__top-icon">
-            <External />
-          </div>
-        </div>
-        <p>
-          Dolor impedit magnam voluptas quo nobis Eius esse explicabo totam
-          eaque eligendi Ut quisquam quas similique fuga atque obcaecati{' '}
-        </p>
       </div>
     </div>,
 
@@ -112,27 +58,9 @@ export default function Projects() {
           src="/../public/images/cards/weather-app.jpeg"
           alt="temp"
           width={450}
-          height={520}
+          height={540}
           objectFit="cover"
         />
-      </div>
-      <div className="cards__content">
-        <div className="cards__top">
-          <div className="cards__top-title">
-            <h3 className="cards__title">
-              <Link href="#">
-                <a className="cards__title-link">Weather App</a>
-              </Link>
-            </h3>
-          </div>
-          <div className="cards__top-icon">
-            <External />
-          </div>
-        </div>
-        <p>
-          Dolor impedit magnam voluptas quo nobis Eius esse explicabo totam
-          eaque eligendi Ut quisquam quas similique fuga atque obcaecati{' '}
-        </p>
       </div>
     </div>,
   ];
@@ -153,9 +81,9 @@ export default function Projects() {
         mouseTracking
         controlsStrategy="alternative"
         disableButtonsControls
-        // animationDuration={1000}
-        // autoPlay
-        // autoPlayInterval={8000}
+        animationDuration={1000}
+        autoPlay
+        autoPlayInterval={8000}
       />
     </section>
   );
