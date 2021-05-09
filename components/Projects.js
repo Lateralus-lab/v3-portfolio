0280e8b1b0a639072ea0754c76d13fa753ff7d02
Lateralus <zoofoo.online@gmail.com>
2021-05-09 17:08:51 +0100

    created different varient of carusel

--- components/Projects.js
+++ components/Projects.js
@@ -5,66 +5,6 @@ import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
 
 export default function Projects() {
-  const responsive = {
-    576: { items: 1 },
-    768: { items: 2 },
-    992: { items: 2 },
-  };
-
-  const items = [
-    <div className="cards__item">
-      <div className="cards__container">
-        <Image
-          className="cards__img"
-          src="/../public/images/cards/chat-app.jpeg"
-          alt="temp"
-          width={450}
-          height={540}
-          objectFit="cover"
-        />
-      </div>
-    </div>,
-
-    <div className="cards__item">
-      <div className="cards__container">
-        <Image
-          className="cards__img"
-          src="/../public/images/cards/loft-taxi.jpeg"
-          alt="temp"
-          width={450}
-          height={540}
-          objectFit="cover"
-        />
-      </div>
-    </div>,
-
-    <div className="cards__item">
-      <div className="cards__container">
-        <Image
-          className="cards__img"
-          src="/../public/images/cards/logistics.jpeg"
-          alt="temp"
-          width={450}
-          height={540}
-          objectFit="cover"
-        />
-      </div>
-    </div>,
-
-    <div className="cards__item">
-      <div className="cards__container">
-        <Image
-          className="cards__img"
-          src="/../public/images/cards/weather-app.jpeg"
-          alt="temp"
-          width={450}
-          height={540}
-          objectFit="cover"
-        />
-      </div>
-    </div>,
-  ];
-
   return (
     <section className="projects">
       <div className="title">
@@ -73,18 +13,162 @@ export default function Projects() {
         </h2>
       </div>
 
-      <AliceCarousel
-        className="cards"
-        items={items}
-        responsive={responsive}
-        infinite
-        mouseTracking
-        controlsStrategy="alternative"
-        disableButtonsControls
-        animationDuration={1000}
-        autoPlay
-        autoPlayInterval={8000}
-      />
+      <div className="projects__item">
+        <div className="cards__wrapper">
+          <div className="cards">
+            <div className="cards__item">
+              <Link href="#">
+                <a className="cards__link">
+                  <div className="cards__icon">
+                    <svg viewbox="0 0 1129 994">
+                      <g
+                        fill="none"
+                        fill-rule="nonzero"
+                        stroke="#999"
+                        stroke-width="41"
+                      >
+                        <path d="M564.5 212.437L95.67 873.5h937.66L564.5 212.437z" />
+                        <path d="M564.5 407.47L163.638 973.5h801.724L564.5 407.47z" />
+                        <path d="M564.5 35.409L39.699 774.5H1089.3L564.5 35.409z" />
+                      </g>
+                    </svg>
+                  </div>
+
+                  <div className="cards__media">
+                    <svg viewbox="0 0 1129 994">
+                      <g
+                        fill="none"
+                        fill-rule="nonzero"
+                        stroke="#F5F5F5"
+                        stroke-width="41"
+                      >
+                        <path d="M564.5 212.437L95.67 873.5h937.66L564.5 212.437z" />
+                        <path d="M564.5 407.47L163.638 973.5h801.724L564.5 407.47z" />
+                        <path d="M564.5 35.409L39.699 774.5H1089.3L564.5 35.409z" />
+                      </g>
+                    </svg>
+                  </div>
+
+                  <div class="cards__header">
+                    <h3 class="cards__header-title">Title of Card</h3>
+                    <p class="cards__header-meta">Subtitle</p>
+                    <div class="cards__header-icon">
+                      <svg viewbox="0 0 28 25">
+                        <path
+                          fill="#fff"
+                          d="M13.145 2.13l1.94-1.867 12.178 12-12.178 12-1.94-1.867 8.931-8.8H.737V10.93h21.339z"
+                        />
+                      </svg>
+                    </div>
+                  </div>
+                </a>
+              </Link>
+            </div>
+          </div>
+
+          <div className="cards">
+            <div className="cards__item">
+              <Link href="#">
+                <a className="cards__link">
+                  <div className="cards__icon">
+                    <svg viewbox="0 0 1129 994">
+                      <g
+                        fill="none"
+                        fill-rule="nonzero"
+                        stroke="#999"
+                        stroke-width="41"
+                      >
+                        <path d="M564.5 212.437L95.67 873.5h937.66L564.5 212.437z" />
+                        <path d="M564.5 407.47L163.638 973.5h801.724L564.5 407.47z" />
+                        <path d="M564.5 35.409L39.699 774.5H1089.3L564.5 35.409z" />
+                      </g>
+                    </svg>
+                  </div>
+
+                  <div className="cards__media">
+                    <svg viewbox="0 0 1129 994">
+                      <g
+                        fill="none"
+                        fill-rule="nonzero"
+                        stroke="#F5F5F5"
+                        stroke-width="41"
+                      >
+                        <path d="M564.5 212.437L95.67 873.5h937.66L564.5 212.437z" />
+                        <path d="M564.5 407.47L163.638 973.5h801.724L564.5 407.47z" />
+                        <path d="M564.5 35.409L39.699 774.5H1089.3L564.5 35.409z" />
+                      </g>
+                    </svg>
+                  </div>
+
+                  <div class="cards__header">
+                    <h3 class="cards__header-title">Title of Card</h3>
+                    <p class="cards__header-meta">Subtitle</p>
+                    <div class="cards__header-icon">
+                      <svg viewbox="0 0 28 25">
+                        <path
+                          fill="#fff"
+                          d="M13.145 2.13l1.94-1.867 12.178 12-12.178 12-1.94-1.867 8.931-8.8H.737V10.93h21.339z"
+                        />
+                      </svg>
+                    </div>
+                  </div>
+                </a>
+              </Link>
+            </div>
+          </div>
+
+          <div className="cards">
+            <div className="cards__item">
+              <Link href="#">
+                <a className="cards__link">
+                  <div className="cards__icon">
+                    <svg viewbox="0 0 1129 994">
+                      <g
+                        fill="none"
+                        fill-rule="nonzero"
+                        stroke="#999"
+                        stroke-width="41"
+                      >
+                        <path d="M564.5 212.437L95.67 873.5h937.66L564.5 212.437z" />
+                        <path d="M564.5 407.47L163.638 973.5h801.724L564.5 407.47z" />
+                        <path d="M564.5 35.409L39.699 774.5H1089.3L564.5 35.409z" />
+                      </g>
+                    </svg>
+                  </div>
+
+                  <div className="cards__media">
+                    <svg viewbox="0 0 1129 994">
+                      <g
+                        fill="none"
+                        fill-rule="nonzero"
+                        stroke="#F5F5F5"
+                        stroke-width="41"
+                      >
+                        <path d="M564.5 212.437L95.67 873.5h937.66L564.5 212.437z" />
+                        <path d="M564.5 407.47L163.638 973.5h801.724L564.5 407.47z" />
+                        <path d="M564.5 35.409L39.699 774.5H1089.3L564.5 35.409z" />
+                      </g>
+                    </svg>
+                  </div>
+
+                  <div class="cards__header">
+                    <h3 class="cards__header-title">Title of Card</h3>
+                    <p class="cards__header-meta">Subtitle</p>
+                    <div class="cards__header-icon">
+                      <svg viewbox="0 0 28 25">
+                        <path
+                          fill="#fff"
+                          d="M13.145 2.13l1.94-1.867 12.178 12-12.178 12-1.94-1.867 8.931-8.8H.737V10.93h21.339z"
+                        />
+                      </svg>
+                    </div>
+                  </div>
+                </a>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
