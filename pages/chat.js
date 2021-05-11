3ad5334244fd9189be5ed70fe196f5201f15f80f
Lateralus <zoofoo.online@gmail.com>
2021-05-11 11:47:08 +0100

    working on pages

--- pages/chat.js
+++ pages/chat.js
@@ -3,7 +3,10 @@ import Layout from '../components/Layout';
 export default function Home() {
   return (
     <Layout>
-      <section className="detail">Chat</section>
+      <section className="detail">
+        <div className="detail__panel">Hello WOrld</div>
+        <div className="detail__panel"></div>
+      </section>
     </Layout>
   );
 }
