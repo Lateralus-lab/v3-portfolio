ed6c7abef4c8c25878343bbbece630629c91db29
Lateralus <zoofoo.online@gmail.com>
2021-05-18 19:36:25 +0100

    added google analytics

--- components/Layout.js
+++ components/Layout.js
@@ -8,6 +8,21 @@ export default function Layout({ title, keywords, description, children }) {
         <meta name="desc" content={description} />
         <meta name="keywords" content={keywords} />
         <link rel="icon" href="/favicon.ico" />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-QLCBG97P04"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-QLCBG97P04');
+        `,
+          }}
+        />
       </Head>
 
       <main className="container">{children}</main>
